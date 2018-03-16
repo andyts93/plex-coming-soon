ba925b3c258e7e88f7b4804f2fe3ac86ef35dcbd
Andrea Castellini <and1@live.it>
2018-03-16 16:23:06 +0100

    Added trailer download and folder creation

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
+from __future__ import unicode_literals
 import requests
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.dirname(__file__)) + '/lib')
 from tmdb3 import set_key, set_locale, Movie
+import youtube_dl
+import re
+import unicodedata
 
 set_key('bee608b6e6519d689c39001fb805090a')
 set_locale('it', 'it')
 
-def get_trailers(imdb_id):
+def my_hook(d):
+	if d['status'] == 'finished':
+		print "Download completato"
+
+ydl_opts = {
+	'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4',
+	'progress_hooks': [my_hook],
+	'ignoreerrors': True,
+	'no_warnings': True,
+	'quiet': True
+}
+
+def get_trailers(imdb_id, retry):
 	movie = Movie(imdb_id)
 	if movie:
 		trailers = movie.youtube_trailers
-		if not trailers:
+		if not trailers and retry == True:
 			# try to get english trailer
 			set_locale('en', 'gb')
-			return get_trailers(imdb_id)
+			return get_trailers(imdb_id, False)
 		else:
 			set_locale('it', 'it')
 			return trailers
@@ -33,8 +49,13 @@ def get_history():
 		else:
 			for item in grabbed:
 				if 'movie' in item and 'tmdbId' in item['movie']:
-					trailers = get_trailers(item['movie']['tmdbId'])
-					print trailers
+					trailers = get_trailers(item['movie']['tmdbId'], True)
+					if trailers:
+						foldername = "%s (%d)" % (item['movie']['title'], item['movie']['year'])
+						ydl_opts['outtmpl'] = '/mnt/hd/trailers/%s/%s.%s' % (foldername, '%(title)s', '%(ext)s')
+						with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+							ydl.download([trailers[0].geturl()])
+						
 
 try:
 	get_history()
