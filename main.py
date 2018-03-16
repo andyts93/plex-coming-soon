3faea4b7be2f79eb21dd07cb4070914439cbf46e
Andrea Castellini <and1@live.it>
2018-03-16 14:44:58 +0100

    Added tmdb parsing and trailer search

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-import feedparser
-import re
-import urllib2
+import requests
 import os
-from bs4 import BeautifulSoup
-import requests;
+import sys
+sys.path.append(os.path.abspath(os.path.dirname(__file__)) + '/lib')
+from tmdb3 import set_key, set_locale, Movie
+
+set_key('bee608b6e6519d689c39001fb805090a')
+set_locale('it', 'it')
+
+def get_trailers(imdb_id):
+	movie = Movie(imdb_id)
+	if movie:
+		trailers = movie.youtube_trailers
+		if not trailers:
+			# try to get english trailer
+			set_locale('en', 'gb')
+			return get_trailers(imdb_id)
+		else:
+			set_locale('it', 'it')
+			return trailers
 
 def get_history():
 	url = "http://192.168.0.20:7878/api/history?page=1&pageSize=10&apikey=326cc12ace3c496791d11f8630ca60f3"
@@ -17,12 +31,16 @@ def get_history():
 		if not grabbed:
 			print "No coming soon titles"
 		else:
-			print grabbed
+			for item in grabbed:
+				if 'movie' in item and 'tmdbId' in item['movie']:
+					trailers = get_trailers(item['movie']['tmdbId'])
+					print trailers
 
 try:
 	get_history()
 except Exception as e:
 	print(e);
+
 # def parseRSS(rss_url):
 # 	return feedparser.parse(rss_url)
 
@@ -30,39 +48,5 @@ except Exception as e:
 # 	for f in os.listdir(dir):
 # 		os.remove(os.path.join(dir, f))
 
-# feed = parseRSS("http://feeds.hd-trailers.net")
-# directory = "/mnt/hd/trailers/"
-# purge_dir(directory)
-# for item in feed['items']:
-# 	title = item['title_detail']['value']
-# 	title = re.sub("[\(\[].*?[\)\]]", "", title)
-# 	title = title.strip()
-# 	for content in item['content']:
-# 		soup = BeautifulSoup(content['value'], "html.parser")
-# 		link = soup.find('a', text='1080p', href=re.compile(".mov$"))
-# 		if link:
-# 			dl_link = link['href']
-# 			filename = title + ".mov"
-# 			if not os.path.exists(directory + filename):
-# 				u = urllib2.urlopen(dl_link)
-# 				f = open(directory + filename, 'wb')
-# 				meta = u.info()
-# 				file_size = int(meta.getheaders("Content-Length")[0])
-# 				print "Downloading %s Bytes: %s" % (filename, file_size)
-
-# 				file_size_dl = 0
-# 				block_sz = 8192
-# 				while True:
-# 					buffer = u.read(block_sz)
-# 					if not buffer:
-# 						break
-# 					file_size_dl += len(buffer)
-# 					f.write(buffer)
-# 					status = r"%10d [%3.2f%%]" % (file_size_dl, file_size_dl * 100 / file_size)
-# 					status = status + chr(8)*(len(status)+1)
-# 					print status,
-# 				f.close()
-# 			else:
-# 				print "%s already exists" % (filename)
 		
 
