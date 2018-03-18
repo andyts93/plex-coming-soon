d7b0b5d20f7952af476c6595bfb02550ba683c98
Andrea Castellini <and1@live.it>
2018-03-18 16:11:57 +0100

    dockerfile

--- Dockerfile
+++ Dockerfile
@@ -9,8 +9,16 @@ RUN pip install requests youtube-dl schedule
 WORKDIR /usr/src/plex-coming-soon
 RUN mkdir logs
 
-COPY config.sample.ini ./lib logger.py main.py plexcomingsoon.py run.sh ./
+COPY ./lib logger.py main.py plexcomingsoon.py run.sh ./
+RUN chmod 755 run.sh
 
 VOLUME /trailers
 
-ENV TMDB_API=""
+ENV RADARR_URL="" \
+    TMDB_API="" \
+    RADARR_API="" \
+    LANG="en" \
+    COUNTRY="gb" \
+    INTERVAL="10"
+
+ENTRYPOINT ["./run.sh"]
--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
-#/bin/bash
-echo "[DEFAULT]" > prova.ini
-echo "tmdb_api_key = ${TMDB_API}" >> prova.ini
+#!/bin/sh
+echo "[DEFAULT]" > config.ini
+echo "radarr_url = ${RADARR_URL}" >> config.ini
+echo "tmdb_api_key = ${TMDB_API}" >> config.ini
+echo "radarr_api_key = ${RADARR_API}" >> config.ini
+echo "language = ${LANG}" >> config.ini
+echo "country = ${COUNTRY}" >> config.ini
+echo "trailer_folder = /trailers" >> config.ini
+echo "interval = ${INTERVAL}" >> config.ini
+
+python main.py &
